90b259e1913643a107f1c9f4f64fcb316fb87c77
Kacha Mukabe <kachamukabe@gmail.com>
2018-01-25 11:31:03 +0200

    updated release tags

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name='devlog',
     packages=['devlog'],
-    version='v0.8',
+    version='v1.0',
     description='A simple command line developer log',
     author='Kacha Mukabe',
     author_email='kmukabe@gmail.com',
     url='https://www.github.com/kachamukabe/devlog',
-    download_url='https://github.com/kachaMukabe/devlog/archive/v0.8.tar.gz',
+    download_url='https://github.com/kachaMukabe/devlog/archive/v1.0.tar.gz',
     keywords=['tag'],
     classifiers=[],
 )
